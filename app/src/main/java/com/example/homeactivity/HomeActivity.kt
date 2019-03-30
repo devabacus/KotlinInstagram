e0404b5b5e22c46fc9a7dbac622b4f67d624f545
devabacusnrf <devabacusnrf@gmail.com>
2019-03-30 20:25:12 +0500

    added loginActivity

--- app/src/main/java/com/example/homeactivity/HomeActivity.kt
+++ app/src/main/java/com/example/homeactivity/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.homeactivity
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
@@ -7,22 +8,31 @@ import com.google.firebase.auth.FirebaseAuth
 class HomeActivity : BaseActivity(0) {
 
     private val TAG = "HomeActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupBottomNavigation()
         Log.d(TAG, "onCreate: ")
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+//        mAuth.signInWithEmailAndPassword("scale-driver@yandex.ru", "123qwe")
+//            .addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    Log.d(TAG, "signIn: success")
+//                } else {
+//                    Log.e(TAG, "signIn: failure", it.exception)
+//                }
+//
+//            }
+    }
 
-        val auth = FirebaseAuth.getInstance()
-        auth.signInWithEmailAndPassword("scale-driver@yandex.ru", "123qwe")
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Log.d(TAG, "signIn: success")
-                } else {
-                    Log.e(TAG, "signIn: failure", it.exception)
-                }
-                
-            }
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
